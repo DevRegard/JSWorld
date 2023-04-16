30e7a07e366087510fe243aece859f2474fbebf2
DevRegard <utr160626@gmail.com>
2023-04-16 21:37:27 +0900

    function refactoring

---
+++ $02_code/level-0/01_7의개수.js
@@ -0,0 +1,17 @@
+// #1
+const solution = (array) => {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    while (num > 0) {
+      if (num % 10 === 7)
+        count++;
+      
+      num = Math.floor(num / 10);
+    }
+  }
+  return count;
+} 
+
+// #2
+const solution_2 = (array) => array.join().split("").filter((el)=>{return el=="7"}).length
